08297cbdb5b1ca19cf2133a03e3287afb2074fa3
Demet Krakoç <karakocdmet1881@gmail.com>
2023-10-15 19:59:09 +0300

    first line added

---
+++ main.py
@@ -0,0 +1,17 @@
+python -m pip install
+
+import pip
+import requests
+import json
+
+def main():
+    req= requests.get('http://pokeapi.co/api/v2/pokemon/1/')
+    print("HTTP Status Code: " + str(req.status_code))
+    print(req.headers)
+    json_response = json.loads(req.content)
+    print("Pokemon Name: " + json_response['name'])
+    print(req.status_code)
+
+req = requests.post('http://api/user', data= None, json=None)
+
+if __name__ == '__main__':
