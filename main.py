56480a5cb9865c9ce7c2ad4658a868876612437e
Onur Mert Anarat <onurmert7213@gmail.com>
2023-10-21 14:14:49 +0300

    deleted json library.

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import requests
-import json
 
 
 def main():
     rsp = requests.get('http://pokeapi.co/api/v2/pokemon/1/')
     if rsp.status_code == 200:
         json_rsp = rsp.json()
-        pokemon_name = json_rsp['name']
-        print(pokemon_name)
+        poke_name = json_rsp['name']
+        print(poke_name)
 
 
 main()
